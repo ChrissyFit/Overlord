4531d815cfb2b137aee51f8dce1a97e542cf6a1b
chrissyfit <gonzalez.christian7777@gmail.com>
2021-11-07 21:22:50 -0500

    muscleman command does not work due to the way heroku is structured. Might find a work around in the future.

--- index.js
+++ index.js
@@ -56,18 +56,22 @@ client.on('interactionCreate', async (interaction) => {
     const { commandName, options } = interaction
 
     if (commandName === 'muscleman') {
-        spawnSync(
-            'python3', 
-            ['muscleman-command/muscleman-img-create.py', options.getString('prompt')]
-        )
+        // spawnSync(
+        //     'python3', 
+        //     ['muscleman-command/muscleman-img-create.py', options.getString('prompt')]
+        // )
 
-        const image = path.join(__dirname, 
-            'muscleman-command/muscleman_meme_temp.png')
+        // const image = path.join(__dirname, 
+        //     'muscleman-command/muscleman_meme_temp.png')
+
+        // interaction.reply({
+        //     files: [{
+        //         attachment: image
+        //     }]
+        // })
 
         interaction.reply({
-            files: [{
-                attachment: image
-            }]
+            content: "Currently unavailable."
         })
 
     }
